9ac5be164594a898d3d63d9081eb2bc135a4c373
emmanuel <emmanuelokon591@gmail.com>
2022-04-20 15:45:50 +0100

    Home screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/providers/style/constants.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -9,7 +12,71 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+    _handleCartItems();
+  }
+
+  Future _handleCartItems() async {}
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage('assets/images/showers.png')),
+                    color: Color(0xffFFFFFF),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10))),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      '0°C',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                      ),
+                    ),
+                    Text(
+                      'San Fransisco',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 21, vertical: 40),
+              child: TextField(
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontFamily: 'poppins',
+                    height: 1.3),
+                decoration: kCountryInputDecorationStyle.copyWith(
+                    labelText: 'Search a Location...',
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.blue,
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
